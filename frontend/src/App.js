e901886ecd2e15f5bce9fde071e3c829b17a569a
hsjin500 <hsjin500@naver.com>
2023-11-07 01:16:59 +0900

    로그 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,8 +34,10 @@ function App() {
         const data = await response.json(); // 이제 안전하게 JSON을 파싱할 수 있음
         if (data.success) { // 서버의 응답에 'success' 키가 있는지 확인
           alert('이미지가 성공적으로 업로드 되었습니다!');
+          console.log('이미지가 성공적으로 업로드 되었습니다.')
         } else {
           alert('업로드에 실패했습니다.');
+          console.log('업로드에 실패했습니다.');
         }
       } else {
         console.error('Server responded with status:', response.status);
