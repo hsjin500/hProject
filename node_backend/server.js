2ff5ad093e26ad65026536e40cf25c5a1e053d33
hsjin500 <hsjin500@naver.com>
2023-11-06 23:48:27 +0900

    add : 이미지 업로드를 위한 서버 라우팅 설정

--- node_backend/server.js
+++ node_backend/server.js
@@ -1,6 +1,9 @@
 const { Pool } = require('pg');
 const express = require('express');
 const path = require('path');
+const multer = require('multer');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const app = express();
 
 const pool = new Pool({
@@ -12,6 +15,47 @@ const pool = new Pool({
 });
 
 const initServer = (app) => {
+
+  // CORS 미들웨어를 전체 앱에 적용
+  const corsOptions = {
+    origin: 'http://144.24.92.232:3000',
+    optionsSuccessStatus: 200
+  };
+
+  app.use(cors(corsOptions));
+  app.use(fileUpload());
+
+  
+  // multer를 사용하여 이미지 저장 설정
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); // 'uploads/' 폴더에 파일 저장
+    },
+    filename: function (req, file, cb) {
+      // 파일 이름 설정 (여기서는 원본 파일 이름 사용)
+      cb(null, file.originalname);
+    }
+  });
+
+  const upload = multer({ storage: storage });
+
+  // 이미지 업로드를 위한 라우트
+  app.post('/upload', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+  
+    // 'sampleFile'은 클라이언트에서 input의 name 속성값입니다.
+    let sampleFile = req.files.sampleFile;
+  
+    // 파일을 'uploads/' 폴더 안에 저장합니다.
+    sampleFile.mv(`${__dirname}/uploads/${sampleFile.name}`, (err) => {
+      if (err)
+        return res.status(500).send(err);
+  
+      res.send('File uploaded!');
+    });
+  });
 
   app.get('/api', (req, res) => {
     pool.query('SELECT * FROM your_table', (err, dbRes) => {
